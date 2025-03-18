b1ba3a5c30f7371440181c58817d8e9d08da12d6
ARNAVLIHANTU23 <Arnav.Lihantu@kalvium.community>
2025-03-18 12:17:29 +0530

    Established MongoDb Connection

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
+
+connectDB();
 
 // Define /ping route
 app.get("/ping", (req, res) => {
